5e2ee46614c07d9155b0457175c17c8af3e2fc97
chhavi <goel.chhavi022@gmail.com>
2018-03-22 13:13:19 +0530

    verifying-jwt-token

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var config = require('./config');
 //configurations
 //port
 var port = process.env.PORT || 3030;
-var apiRoutes = require('./apiRoutes');
 
 //bodyparser to get post data or data from url 
 app.use(bodyParser.urlencoded({extended : false}));
@@ -27,71 +26,101 @@ app.set('secret', config.secret);
 
 app.use("/", apiRoutes);
 
-apiRoutes.get('/', (req, res, next) => {
-    res.json({success : true});
+//verifying
+apiRoutes.use((req, res, next) => {
+
+    let token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+    if(token){
+        jsonwebtoken.verify(token, app.get('secret'), (err, decode) => {
+            if(err){
+                return res.json({
+                    success : false,
+                    message : "Failed to authenticate token"
+                });
+            }
+            else {
+                req.decode = decode;
+                next();
+            }
+        });
+    }
+    else {
+        return res.json({
+            success : false,
+            message : "No token provided"
+        });
+    }
+
 });
 
-apiRoutes.get('/setup', function(req, res){
-    var chhavi = new User({
-        userName : "Chhavi",
-        password : "chhavi123",
-        admin : true
-    });
+//apis
 
-    chhavi.save(function(err){
-        if(err) throw err;
-        console.log("User saved successfully");
-        res.json({ success : true });
-    });
-});
+// apiRoutes.get('/setup', function(req, res){
+//     var chhavi = new User({
+//         userName : "Chhavi",
+//         password : "chhavi123",
+//         admin : true
+//     });
 
-//get all users
-apiRoutes.get('/users', function(req, res){
-    User.find({}, (err, users) => {
-        if(err) throw err;
-        res.json(users);
-    });
-});
+//     chhavi.save(function(err){
+//         if(err) throw err;
+//         console.log("User saved successfully");
+//         res.json({ success : true });
+//     });
+// });
+
+// apiRoutes.get('/', (req, res, next) => {
+//     res.json({success : true});
+// });
+
+// //get all users
+// apiRoutes.get('/users', function(req, res){
+//     User.find({}, (err, users) => {
+//         if(err) throw err;
+//         res.json(users);
+//     });
+// });
 
 //get one
-apiRoutes.post('/users', (req, res) => {
-    User.findOne({
-        userName : req.query.userName
-    }, (err, user) => {
-        if(err) throw err;
-        if(!user){
-            res.json({ 
-                success: false, 
-                message: 'Authentication failed. User not found.' 
-            });
-        }
-        else if(user){
-            //check if password is correct or not
-            if(user.password != req.query.password){
-                res.json({ 
-                    success: false, 
-                    message: 'Authentication failed. Wrong password.' 
-                });
-            }
-            else{
-                const payload = {
-                    admin : user.admin
-                };
+// apiRoutes.post('/users', (req, res) => {
+//     User.findOne({
+//         userName : req.query.userName
+//     }, (err, user) => {
+//         if(err) throw err;
+//         if(!user){
+//             res.json({ 
+//                 success: false, 
+//                 message: 'Authentication failed. User not found.' 
+//             });
+//         }
+//         else if(user){
+//             //check if password is correct or not
+//             if(user.password != req.query.password){
+//                 res.json({ 
+//                     success: false, 
+//                     message: 'Authentication failed. Wrong password.' 
+//                 });
+//             }
+//             else{
+//                 const payload = {
+//                     admin : user.admin
+//                 };
 
-                //create-token
-                var token = jsonwebtoken.sign(payload, app.get('secret'), {
-                    expiresIn: 24*60*60      // expires in 24 hours
-                });
+//                 //create-token
+//                 var token = jsonwebtoken.sign(payload, app.get('secret'), {
+//                     expiresIn: 24*60*60      // expires in 24 hours
+//                 });
 
-                res.json({
-                    success : true,
-                    message : "Token generated",
-                    token : token
-                });
-            }
-        }
-    });
-});
+//                 res.json({
+//                     success : true,
+//                     message : "Token generated",
+//                     //token : token
+//                 });
+//             }
+//         }
+//     });
+// });
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
