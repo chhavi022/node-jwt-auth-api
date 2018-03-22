540f030ce74239ec19e39b98728a0649bae28b26
chhavi <goel.chhavi022@gmail.com>
2018-03-22 11:34:02 +0530

    generated token

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,91 +1,84 @@
-var apiRoutes = require('express').Router();
+//you can also create a separate module for routing 
+//but i was getting an error in setting buffer or string to app so i just embedded all in app.
 
-var mongoose = require('mongoose');
-var config = require('./config');
-var User = require('./models/user');
 
-//connect to database
-mongoose.connect(config.database);
+// var api = require('express');
+// var apiRoutes = api.Router();
+// var mongoose = require('mongoose');
+// var jsonwebtoken = require('jsonwebtoken');                 //to create & verify jwt
+// var config = require('./config');
+// var User = require('./models/user');
 
-const MongoClient = require('mongodb').MongoClient;
+// //connect to database
+// mongoose.connect(config.database);
 
-const url = 'mongodb://localhost:27017/User';
+// //set secret code to app
+// api().set('secret', config.secret);
 
-var emp_data = [];
-
-apiRoutes.get('/', (req, res, next) => {
-    res.json({success : true});
-});
-
-apiRoutes.get('/setup', function(req, res){
-    var chhavi = new User({
-        userName : "Chhavi",
-        password : "chhavi123",
-        admin : true
-    });
-
-    chhavi.save(function(err){
-        if(err) throw err;
-        console.log("User saved successfully");
-        res.json({ success : true });
-    });
-});
-
-//get all users
-apiRoutes.get('/users', function(req, res){
-    User.find({}, (err, users) => {
-        if(err) throw err;
-        res.json(users);
-    });
-});
-
-//get one
-apiRoutes.post('/users', (req, res) => {
-    User.findOne({
-        userName : req.body.userName
-    }, (err, user) => {
-        if(err) throw err;
-        if(!user){
-            res.json({ 
-                success: false, 
-                message: 'Authentication failed. User not found.' 
-            });
-        }
-        else if(user){
-            //check if password is correct or not
-            if(user.password != req.body.password){
-                res.json({ 
-                    success: false, 
-                    message: 'Authentication failed. Wrong password.' 
-                });
-            }
-            else{
-                const payload = {
-                    admin : user.admin
-                };
-
-                //create-token
-                var token = jwt.sign(payload, app.get('secret'), {
-                    expiresInMinutes: 1440      // expires in 24 hours
-                });
-
-                res.json({
-                    success : true,
-                    message : "Token generated",
-                    token : token
-                });
-            }
-        }
-        res.json(user);
-    });
-});
-
-// apiRoutes.put('/', (req, res, next) => {
-    
+// apiRoutes.get('/', (req, res, next) => {
+//     res.json({success : true});
 // });
 
-// apiRoutes.delete('/', (req, res, next) => {
-    
+// apiRoutes.get('/setup', function(req, res){
+//     var chhavi = new User({
+//         userName : "Chhavi",
+//         password : "chhavi123",
+//         admin : true
+//     });
+
+//     chhavi.save(function(err){
+//         if(err) throw err;
+//         console.log("User saved successfully");
+//         res.json({ success : true });
+//     });
 // });
 
-module.exports = apiRoutes;
+// //get all users
+// apiRoutes.get('/users', function(req, res){
+//     User.find({}, (err, users) => {
+//         if(err) throw err;
+//         res.json(users);
+//     });
+// });
+
+// //get one
+// apiRoutes.post('/users', (req, res) => {
+//     User.findOne({
+//         userName : req.query.userName
+//     }, (err, user) => {
+//         if(err) throw err;
+//         if(!user){
+//             res.json({ 
+//                 success: false, 
+//                 message: 'Authentication failed. User not found.' 
+//             });
+//         }
+//         else if(user){
+//             //check if password is correct or not
+//             if(user.password != req.query.password){
+//                 res.json({ 
+//                     success: false, 
+//                     message: 'Authentication failed. Wrong password.' 
+//                 });
+//             }
+//             else{
+//                 const payload = {
+//                     admin : user.admin
+//                 };
+
+//                 //create-token
+//                 var token = jsonwebtoken.sign(payload, api().get('secret'), {
+//                     expiresIn: 24*60*60      // expires in 24 hours
+//                 });
+
+//                 res.json({
+//                     success : true,
+//                     message : "Token generated",
+//                     token : token
+//                 });
+//             }
+//         }
+//     });
+// });
+
+// module.exports = apiRoutes;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //packages
 const app = require('express')();
-
-var mongoose = require('mongoose');                         //to interact with database
+var apiRoutes = require('express').Router();
+var mongoose = require('mongoose');
 var morgan = require('morgan');                             //log requests to the console so we can see what is happening
 var bodyParser = require('body-parser');                    //let us get parameters from our POST requests
-var jsonwebtoken = require('jsonwebtoken');                 //to crreate & verify jwt
+var jsonwebtoken = require('jsonwebtoken');                 //to create & verify jwt
 
+var User = require('./models/user');                       //to interact with database
 var config = require('./config');
 
 //configurations
@@ -13,15 +14,84 @@ var config = require('./config');
 var port = process.env.PORT || 3030;
 var apiRoutes = require('./apiRoutes');
 
-//set secret code to app
-app.set('secret', config.secret);
-
 //bodyparser to get post data or data from url 
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+//connect to database
+mongoose.connect(config.database);
+
+//set secret code to app
+app.set('secret', config.secret);
+
 app.use("/", apiRoutes);
+
+apiRoutes.get('/', (req, res, next) => {
+    res.json({success : true});
+});
+
+apiRoutes.get('/setup', function(req, res){
+    var chhavi = new User({
+        userName : "Chhavi",
+        password : "chhavi123",
+        admin : true
+    });
+
+    chhavi.save(function(err){
+        if(err) throw err;
+        console.log("User saved successfully");
+        res.json({ success : true });
+    });
+});
+
+//get all users
+apiRoutes.get('/users', function(req, res){
+    User.find({}, (err, users) => {
+        if(err) throw err;
+        res.json(users);
+    });
+});
+
+//get one
+apiRoutes.post('/users', (req, res) => {
+    User.findOne({
+        userName : req.query.userName
+    }, (err, user) => {
+        if(err) throw err;
+        if(!user){
+            res.json({ 
+                success: false, 
+                message: 'Authentication failed. User not found.' 
+            });
+        }
+        else if(user){
+            //check if password is correct or not
+            if(user.password != req.query.password){
+                res.json({ 
+                    success: false, 
+                    message: 'Authentication failed. Wrong password.' 
+                });
+            }
+            else{
+                const payload = {
+                    admin : user.admin
+                };
+
+                //create-token
+                var token = jsonwebtoken.sign(payload, app.get('secret'), {
+                    expiresIn: 24*60*60      // expires in 24 hours
+                });
+
+                res.json({
+                    success : true,
+                    message : "Token generated",
+                    token : token
+                });
+            }
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
